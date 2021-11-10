b7478f7a88bf49779af336ef72ad813870212684
Brandon Chuck <blc42310@gmail.com>
2021-11-09 16:06:23 -0800

    Refactored updateClock function by reorganizing logic into dedicated functions.

--- clock.js
+++ clock.js
@@ -1,100 +1,88 @@
-//Assign days of the week to 0-6 values returned by dayOfWeek() method
-var weekdays = new Array(7);
-weekdays[0] = "Monday";
-weekdays[1] = "Tuesday";
-weekdays[2] = "Wednesday";
-weekdays[3] = "Thursday";
-weekdays[4] = "Friday";
-weekdays[5] = "Saturday";
-weekdays[6] = "Sunday";
-
-//Assign months to 0-11 values returned by getMonth() method
-var months = new Array(12);
-months[0] = "January";
-months[1] = "February";
-months[2] = "March";
-months[3] = "April";
-months[4] = "May";
-months[5] = "June";
-months[6] = "July";
-months[7] = "August";
-months[8] = "September";
-months[9] = "October";
-months[10] = "November";
-months[11] = "December";
-
-// Date suffix values -- do not need one for "th"; this would be the longest array anyways!
-var dateSuffix_st = [1,21,31]
-var dateSuffix_nd = [2,22]
-var dateSuffix_rd = [3,23]
-
-
-// create time h1 and date h2
-var timeElement = document.createElement('h1');
-timeElement.id = 'clock';
-var dateElement = document.createElement('h1');
-dateElement.id = 'date';
-
-// Want: DayOfWeek, Month Day Year
-
-// Start the clock
-function startClock(){
-    setInterval(updateClock, 1000); // call updateClock every seconds
-}
-
-// Update time every second
 function updateClock(){
+    const timeObject = new Date();
     
-    console.log("Ticking!");
+    const hours = timeObject.getHours();
+    const minutes = appendLeadingZero(timeObject.getMinutes());
+    const seconds = appendLeadingZero(timeObject.getSeconds());
 
-    // Create new date object each time 
-    var theDate = new Date();
-    
+    const isAm = hours < 12 || hours === 0;  // if hours === 0, then it's midnight
+    const amPm = isAm ? 'AM' : 'PM';
 
-    //  ------ TIME Structure ------ 
-    // grab current time properties for new date obj
-    var hours = theDate.getHours();
-    var minutes = theDate.getMinutes();
-    var seconds = theDate.getSeconds();
-    var am_pm = '';
+    return document.querySelector('#clock-container').textContent = `${appendLeadingZero(formatHours(hours))}:${minutes}:${seconds} ${amPm}`;
 
-    hours = hours < 10 ? '0' +  hours : hours;         // if hours<10 append 0, otherwise just set to getHours() 
-    minutes = minutes < 10 ? '0' +  minutes : minutes; // if mins<10 append 0, otherwise just set to getMinutes()
-    seconds = seconds < 10 ? '0' + seconds : seconds;  // if seconds<10 append 0, otherwise just set to getSeconds()
-    am_pm = hours < 12 ? am_pm = 'AM' : am_pm = 'PM';  // toggle AM/PM based on hours
-
-    // ------ DATE Structure ------ 
-    var dayOfWeek = weekdays[theDate.getDay()]; // Mon, Tues, Wed, etc.
-    var month = months[theDate.getMonth()]; // Jan, Feb, March, etc.
-    var day = theDate.getDate(); // 21st, 22nd, 23rd etc.
-    var year = theDate.getFullYear();
-
-    // Handle day suffix
-    if (dateSuffix_nd.includes(day)){
-        day = day + 'nd';
-    } else if (dateSuffix_rd.includes(day)){
-        day = day + 'rd';
-    } else if (dateSuffix_st.includes(day)) {
-        day = day + 'st';
-    } else {
-        day = day + 'th';
-    }
-
-
-    // Reference to clock and date divs
-    var clock = document.querySelector('#clock-container');
-    var date = document.querySelector('#date-container');
-    
-    // Set time text
-    timeElement.textContent = `${hours}:${minutes}:${seconds} ${am_pm}`;
-
-    // Set date text
-    dateElement.textContent = `${dayOfWeek}, ${month} ${day} ${year}`;
-
-    // Append updated timeElement & dateElement to clock and date divs
-    clock.appendChild(timeElement);
-    date.appendChild(dateElement);
 }
 
-// Start the Clock
-startClock();
+function updateDate(){
+        const dateObject = new Date();
+
+        const dayOfWeek = weekdays[dateObject.getDay()];
+        const month = months[dateObject.getMonth()]; 
+        const date = appendDateSuffix(dateObject.getDate()); // 1st, 2nd, 3rd, 4th 
+        const year = dateObject.getFullYear();
+
+        return document.querySelector('#date-container').textContent = `${dayOfWeek}, ${month} ${date} ${year}`;
+}
+
+
+function appendLeadingZero(num){
+    return num < 10 ? '0' + num : num;
+}
+
+// 12-hour clock functionality
+function formatHours(hours){
+    hours = hours > 12 ? hours - 12 : hours;
+    hours = hours === 0 ? hours + 12 : hours;
+
+    return hours;
+}
+
+function appendDateSuffix(date){
+
+    // skip 11-19
+    if (date < 10 || date > 20){
+        switch(date % 10){
+            case 1:
+                return `${date}st`;
+            case 2:
+                return `${date}nd`;
+            case 3:
+                return `${date}rd`;
+        }
+    }
+    return `${date}th`;
+}
+
+const weekdays = [
+    "Tuesday",
+    "Monday",
+    "Thursday",
+    "Wednesday",
+    "Saturday",
+    "Friday",
+    "Sunday"
+];
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+// Call to prevent lag in loading clock/date
+updateClock();
+updateDate();
+
+// Start clock
+setInterval(() => {
+    updateClock();
+    updateDate();
+}, 1000);
